f32a1fd95656b6e9e69100e98b05764d2a27895e
polaris <61411085+polaris428@users.noreply.github.com>
2022-05-24 11:55:28 +0900

    필요없는 쉘 삭제
    필요없는 쉘 삭제

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from turtle import st
 import pandas as pd
 
 data = pd.read_csv('steam_games.csv',sep=',')
-
+data = data[data['types'] == 'app']
+data.drop(columns = ['url','types','name','recent_reviews','desc_snippet','publisher','game_description','minimum_requirements','recommended_requirements','popular_tags','discount_price'],inplace=True) 
 print("출력시작") 
 
 print(data)
