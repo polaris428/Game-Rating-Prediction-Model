d570996e5d417e1780648c45c381c59b7219c3fe
polaris <61411085+polaris428@users.noreply.github.com>
2022-05-24 12:00:35 +0900

    전처리 후 값 저장
    전처리 후 값 저장

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ import pandas as pd
 data = pd.read_csv('steam_games.csv',sep=',')
 data = data[data['types'] == 'app']
 data.drop(columns = ['url','types','name','recent_reviews','desc_snippet','publisher','game_description','minimum_requirements','recommended_requirements','popular_tags','discount_price'],inplace=True) 
-print("출력시작") 
+#전처리를
+data["original_price"]=data["original_price"].astype('str').str.replace("Free","$0")
 
+data["original_price"]=data["original_price"].astype('str').str.replace("$","")
+print("출력시작") 
+  
 print(data)
+#파일로 저장
+data.to_csv(
+    path_or_buf = '전처리 완료후.csv',
+  
+)
 
