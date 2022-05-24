9ab351ac2705d5af64ae3d612cb2ff56399d26cd
polaris <61411085+polaris428@users.noreply.github.com>
2022-05-24 16:15:22 +0900

    주석 추가
    주석 추가

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 from turtle import st
 import pandas as pd
 import string
-
+import re   
 data = pd.read_csv('steam_games.csv',sep=',')
 data = data[data['types'] == 'app']
 data.drop(columns = ['url','types','name','recent_reviews','desc_snippet','publisher','game_description','minimum_requirements','recommended_requirements','popular_tags','discount_price'],inplace=True) 
 #전처리를 시작
 
-
 data["all_reviews"]=data["all_reviews"].str.extract(r'(\d+%)')
 data["all_reviews"]=data["all_reviews"].astype('str').str.replace("%","")
 
-#original_price=data["all_reviews"].isin(["nan"])
-#data=data[~original_price]
+#nan이 들어있는 값 삭세
 data=data[data.original_price!="nan"]
 data=data[data.all_reviews!="nan"]
+#original_price 전처리
 data["original_price"]=data["original_price"].astype('str').str.replace("Free","$0")
 data["original_price"]=data["original_price"].astype('str').str.replace("0 To Play ","$0")
 data["original_price"]=data["original_price"].astype('str').str.replace("$","")
-
+#developer 전처리
 data["developer"]=data["developer"].astype('str').str.strip(string.punctuation)
-
+#languages 전처리
 data["languages"]=data["languages"].astype('str').str.split(",")
-
-
-    
 
 
 print("출력시작") 
